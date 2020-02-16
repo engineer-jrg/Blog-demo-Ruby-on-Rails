0da2980c9e14e96e816eb5beb8536884e7af937d
enginer-jrg <engineer.jrg@gmail.com>
2020-02-15 20:37:50 -0500

    cambios en production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # For mailing
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "https://blog-ruby-rails.herokuapp.com/" }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = "https://blog-ruby-rails.herokuapp.com/"
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
